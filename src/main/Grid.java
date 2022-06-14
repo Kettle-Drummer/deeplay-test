98f4c8fc660a5b9fd0673b3fa36a92ddb49b3683
Istomin Alex <58596713+Kettle-Drummer@users.noreply.github.com>
2022-06-14 11:38:36 +0600

    Update Grid.java

--- src/main/Grid.java
+++ src/main/Grid.java
@@ -6,12 +6,12 @@ import java.util.List;
 public class Grid {
 
 	public Node[][] nodes;
-    int gridWidth, gridHeight;
+    int gridSize;
 
     public Grid(int size, int[][] tile_costs) 
     	{    	
-        	gridWidth = size;
-        	gridHeight = size;
+        	gridSize = size;
+
         	nodes = new Node[size][size];
 
         	for (int i = 0; i < size; i++)
@@ -24,10 +24,10 @@ public class Grid {
     	{
         	List<Node> neighbours = new ArrayList<Node>();
 
-        	if (node.row + 1 >= 0 && node.row + 1  < gridHeight) neighbours.add(nodes[node.col][node.row + 1]);
-        	if (node.row - 1 >= 0 && node.row - 1  < gridHeight) neighbours.add(nodes[node.col][node.row - 1]);
-        	if (node.col + 1 >= 0 && node.col + 1  < gridHeight) neighbours.add(nodes[node.col + 1][node.row]);
-        	if (node.col - 1 >= 0 && node.col - 1  < gridHeight) neighbours.add(nodes[node.col - 1][node.row]);
+        	if (node.row + 1 >= 0 && node.row + 1  < gridSize) neighbours.add(nodes[node.col][node.row + 1]);
+        	if (node.row - 1 >= 0 && node.row - 1  < gridSize) neighbours.add(nodes[node.col][node.row - 1]);
+        	if (node.col + 1 >= 0 && node.col + 1  < gridSize) neighbours.add(nodes[node.col + 1][node.row]);
+        	if (node.col - 1 >= 0 && node.col - 1  < gridSize) neighbours.add(nodes[node.col - 1][node.row]);
         
         	return neighbours;
     	}
